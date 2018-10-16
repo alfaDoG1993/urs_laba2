f763fdb1af3e56412be27d336502244495c9a81c
Leonid <leonid_tsyba@mail.ru>
2018-10-16 16:39:59 +0300

    TsybLeon@ 777  13345/2 add lab2

---
+++ begin_srv/include/Name.h
@@ -0,0 +1,21 @@
+#ifndef CITATIONS_LIST_H
+#define CITATIONS_LIST_H
+
+#include <map>            // Контейнер для создания словаря
+#ifndef RUSSIAN
+//#define RUSSIAN
+std::map <std::string, std::string> name_list = {
+  {"Vasya Petrov 03923235452", "Vasya Petrov"},
+  {"Ivan Zarechni 13923235452", "Ivan Zarechni"},
+  {"Dan Kozlovski 23923235452", "Dan Kozlovski"},
+  {"Vasya Knopkin 33123324234", "Vasya Knopkin"},
+  {"Vasya Petrov 4423235452", "Vasya Petrov"},
+  {"Nastya Zvonova 5523235452", "Nastya Zvonova"},
+  {"Kate Stypova 6623235452", "Kate Stypova"},
+  {"Serg Kolashov 77124342354", "Serg Kolashov"},
+  {"Konstantin Habenski 8812432354", "Konstantin Habenski"},
+  {"Ledi Gaga 9912432354", "Ledi_Gaga"},
+  {"Bredli Cooper 1034234234", "Bredli Cooper"},
+};
+#endif
+#endif // CITATIONS_LIST_H
